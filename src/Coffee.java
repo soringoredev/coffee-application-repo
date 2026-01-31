9edd2f347fc3ed76a99a4c64b773ef8999cdd704
Sorin Gore <soringoredev@gmail.com>
2026-01-31 17:06:23 +0200

    feat: implement Coffee class

---
+++ src/Coffee.java
@@ -0,0 +1,31 @@
+public class Coffee {
+    // props
+    private int temp;
+    private int conc;
+
+    //constructor (init)
+
+    public Coffee(int t, int c) {
+        temp = t;
+        conc = c;
+    }
+
+    // getters
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public int getConc() {
+        return conc;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Coffee{" +
+                "temperature=" + temp +
+                ", concentration=" + conc +
+                '}';
+    }
+}
